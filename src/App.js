7b9e503e08fa6fb3356f9c51245ad6b7449702ee
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-03-10 13:52:57 +0000

    phase 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
+  );
 }
 
 export default App;
