5f91eead180e8dd2a0f68bb50225e5a3dabe3d67
RAJ66 <vitorbarbosa1998@hotmail.com>
2020-03-10 16:26:58 +0000

    phase 3

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,13 @@
 import React from "react";
 
-export default function TodoItem() {
+export default function TodoItem(props) {
+  console.log(props);
   return (
     <div className="todo-item">
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <input type="checkbox" checked={props.item.completed} />
+      <p style={props.item.completed ? { color: "red" } : { color: "black" }}>
+        {props.item.text}
+      </p>
     </div>
   );
 }
